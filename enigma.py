9cc498f31db90a8d5286642294e9218eb6ec5c36
danhab99 <dan.habot@gmail.com>
2018-04-13 18:02:24 +0300

    Cleanup

--- enigma.py
+++ enigma.py
@@ -3,7 +3,7 @@ import argparse
 import sys, os
 
 parser = argparse.ArgumentParser(description='A simulation of the enigma encryption algorithm', prog='enigma.py')
-subparsers = parser.add_subparsers(help='Which command to run')
+subparsers = parser.add_subparsers(help='Which command to run', dest='subroutine')
 
 create_parser = subparsers.add_parser('create', help='A utility to create encryption codexes')
 encrypt_parser = subparsers.add_parser('encrypt', help='Encrypt a file with a codex')
@@ -24,11 +24,21 @@ encrypt_parser.add_argument('out_file',
     metavar='<Out file>',
     type=argparse.FileType('w'),
     help='The destination for the resuts')
-encrypt_parser.add_argument('--codex',
+
+encrypt_mutual = encrypt_parser.add_mutually_exclusive_group()
+encrypt_mutual.add_argument('--codex',
     type=argparse.FileType('r'),
     help='The codex to use')
-encrypt_parser.add_argument('--random',
+encrypt_mutual.add_argument('--random',
     action='store_true',
     help='Create a random codex')
 
 args = parser.parse_args()
+
+# print(args)
+
+if (args.subroutine == 'create'):
+    print('Create subroutine')
+
+if (args.subroutine == 'encrypt'):
+    print('Encrypt subroutine')
