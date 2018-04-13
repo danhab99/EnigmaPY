020f69a76186086919354307b8af4345910c689d
danhab99 <dan.habot@gmail.com>
2018-04-13 22:22:16 +0300

    Whoops

--- enigma/rotor.py
+++ enigma/rotor.py
@@ -2,7 +2,7 @@ import transformer
 
 class Rotor(transformer.Transformer):
 
-    def __init__(self, abc cypher, initPos):
+    def __init__(self, abc, cypher, initPos):
         self.cypher = cypher
         self.offset = initPos
         self.cypher = cypher
