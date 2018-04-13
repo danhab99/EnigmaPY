db585002b108e7692e49a1dbaa93f4dfeaeca314
danhab99 <dan.habot@gmail.com>
2018-04-13 17:03:26 +0300

    Ok this is how I want the parameters

--- enigma.py
+++ enigma.py
@@ -2,20 +2,33 @@ from enigma import *
 import argparse
 import sys, os
 
-parser = argparse.ArgumentParser(description='A simulation of the enigma encryption algorithm', prog="enigma.py")
-parser.add_argument('command',
-    type=str,
-    choices=['encrypt', 'create'],
-    help='Which subroutine to run')
-parser.add_argument('in_file',
+parser = argparse.ArgumentParser(description='A simulation of the enigma encryption algorithm', prog='enigma.py')
+subparsers = parser.add_subparsers(help='Which command to run')
+
+create_parser = subparsers.add_parser('create', help='A utility to create encryption codexes')
+encrypt_parser = subparsers.add_parser('encrypt', help='Encrypt a file with a codex')
+
+create_parser.add_argument('file',
+    metavar='<File>',
+    type=argparse.FileType('w'),
+    help='The file to output to')
+create_parser.add_argument('-r, --random',
+    action='store_true',
+    help='Generates a completly random codex')
+
+encrypt_parser.add_argument('in_file',
     metavar='<Input file>',
     type=argparse.FileType('r'),
     help='The file to be encrypted')
-parser.add_argument('out_file',
-    metavar='<Output file>',
+encrypt_parser.add_argument('out_file',
+    metavar='<Out file>',
     type=argparse.FileType('w'),
-    help='The output file')
+    help='The destination for the resuts')
+encrypt_parser.add_argument('--codex',
+    type=argparse.FileType('r'),
+    help='The codex to use')
+encrypt_parser.add_argument('--random',
+    action='store_true',
+    help='Create a random codex')
 
 args = parser.parse_args()
-
-print(args.command)
