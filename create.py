e9a8cd9775dbeccfbc266b9df77db50a89546a18
danhab99 <dan.habot@gmail.com>
2018-04-13 22:21:56 +0300

    Lots of work

--- create.py
+++ create.py
@@ -15,33 +15,60 @@ def genPreset(p):
         return [str(char(i)) for i in range(0, int(0x10ffff))]
 
 def Create():
-    parser = argparse.ArgumentParser(description='This interactive utility is used to help you create a custom codex for the encryptor')
+    parser = argparse.ArgumentParser(description='This interactive utility is used to help you create a custom cypher for the encryptor')
     abc_mutual = parser.add_mutually_exclusive_group()
     abc_mutual.add_argument('--abc', help='Manually define an alphabet to be used')
     abc_mutual.add_argument('-p, --preset',
         help='Use one of the predefined alphabets',
         choices=['ABC', 'bytes', 'numbers', 'ASCII', 'UTF'],
-        default='bytes')
+        default='bytes',
+        dest='preset')
 
-    create_rotor = parser.add_subparsers('rotor', help='Rotors are transformers that rotate their codex by one place per each use')
-    create_plugboard = parser.add_subparsers('plugboard', help='Plugboards simply switch one symbol for another')
-    end = parser.add_subparsers('end', help='Finalizes the codex')
+    parser.print_usage()
 
-    def addCodexArg(i):
-        i.add_argument('-c, --codex',
-            help='The codex to use')
+    def prompt():
+        return parser.parse_args(input("create ").split())
+
+    temp = prompt()
+    ABC = temp.abc
+
+    if (temp.preset is not None and temp.ABC is None):
+        ABC = genPreset(temp.prompt)
+
+
+    parser = argparse.ArgumentParser(description='This interactive utility is used to help you create a custom cypher for the encryptor')
+    subparsers = parser.add_subparsers('command', help='Which command to run', dest='subroutine')
+
+    create_rotor = subparsers.add_subparsers('rotor', help='Rotors are transformers that rotate their cypher by one place per each use')
+    create_plugboard = subparsers.add_subparsers('plugboard', help='Plugboards simply switch one symbol for another')
+    end = subparsers.add_subparsers('end', help='Finalizes the cypher')
+
+    def addcypherArg(i):
+        i.add_argument('-c, --cypher',
+            help='The cypher to use')
 
     create_rotor.add_argument('-i, --initial',
         help='The initial rotor position to start at (default = 0)',
         type=int,
         default=0)
 
-    addCodexArg(create_rotor)
-    addCodexArg(create_plugboard)
+    addcypherArg(create_rotor)
+    addcypherArg(create_plugboard)
 
     parser.print_usage()
     keepRunning = True
 
     while(keepRunning):
-        args = parser.parse_args(input(">"))
-        
+        args = prompt()
+
+        if (args.subroutine == 'rotor'):
+            yield enigma.Rotor(abc=ABC, cypher=args.cypher, initPos=args.initial)
+            continue
+
+        if (args.subroutine == 'plugboard'):
+            yield enigma.Plugboard(abc=ABC, cypher=args.cypher)
+            continue
+
+        if (args.subroutine == 'end')
+            keepRunning = False
+            continue
